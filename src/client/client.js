afc069999eacf6be5459ee344b50657c6c45476f
girijapathi.math@gmail.com <girijapathi.math@gmail.com>
2020-06-08 14:15:25 +0530

    routing initiated.

--- src/client/client.js
+++ src/client/client.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
-
-ReactDOM.hydrate(<Home />, document.querySelector("#root"));
+import Routes from './Routes';
+import { BrowserRouter } from 'react-router-dom';
+ReactDOM.hydrate(<BrowserRouter>
+<Routes /></BrowserRouter>, document.querySelector("#root"));
